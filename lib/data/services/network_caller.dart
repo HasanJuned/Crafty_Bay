429c03b667a412d412a01623a8454bbe3c16101f
Hassan <hasanahmad9812021@gmail.com>
2023-10-12 03:18:42 +0600

    Network work caller integrated

---
+++ lib/data/services/network_caller.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'package:http/http.dart';
+
+import '../models/network_response.dart';
+
+class NetworkCaller {
+  /// get request method
+  static Future<NetworkResponse> getRequest(String url) async {
+    try {
+      Response response = await get(Uri.parse(url),
+          headers: {
+            // 'token': AuthController.accessToken.toString()
+          });
+      log(response.statusCode.toString());
+      log(response.body);
+      if (response.statusCode == 200) {
+        return NetworkResponse(
+            true, response.statusCode, jsonDecode(response.body));
+      } else if (response.statusCode == 401) {
+        gotoLogin();
+      } else {
+        return NetworkResponse(false, response.statusCode, null);
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return NetworkResponse(false, -1, null);
+  }
+
+  static Future<NetworkResponse> postRequest(String url,
+      Map<String, dynamic> body,
+      {bool isLogin = false}) async {
+    try {
+      Response response = await post(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+          //'token': AuthController.accessToken.toString()
+        },
+        body: jsonEncode(body),
+      );
+      log(response.statusCode.toString());
+      log(response.body);
+      if (response.statusCode == 200) {
+        return NetworkResponse(
+          true,
+          response.statusCode,
+          jsonDecode(response.body),
+        );
+      } else if (response.statusCode == 401) {
+        if (isLogin == false) {
+          gotoLogin();
+        }
+      } else {
+        return NetworkResponse(false, response.statusCode, null);
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return NetworkResponse(false, -1, null);
+  }
+
+  static Future<void> gotoLogin() async {
+    //   await AuthController.clear();
+    //   Navigator.pushAndRemoveUntil(
+    //       CraftBay.globalKey.currentContext!,
+    //       MaterialPageRoute(builder: (context) => const EmailVerificationScreen()),
+    //           (route) => false);
+    // }
+  }
+}
