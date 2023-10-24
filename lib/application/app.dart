41f24f09cc11fe8301399828447e7c8fef9a700b
Hassan <hasanahmad9812021@gmail.com>
2023-10-24 11:36:35 +0600

    Check Internet connection has been implemented

--- lib/application/app.dart
+++ lib/application/app.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:crafty_bay/presentation/state_holders/main_bottom_nav_bar_controller.dart';
 import 'package:crafty_bay/presentation/ui/screens/splash_screen.dart';
 import 'package:crafty_bay/presentation/ui/utility/app_colors.dart';
@@ -8,6 +11,7 @@ import 'state_holder_binders.dart';
 
 class CraftyBay extends StatefulWidget {
   static GlobalKey<NavigatorState> globalKey = GlobalKey<NavigatorState>();
+
   const CraftyBay({Key? key}) : super(key: key);
 
   @override
@@ -16,6 +20,41 @@ class CraftyBay extends StatefulWidget {
 
 class _CraftyBayState extends State<CraftyBay> {
   @override
+  void initState() {
+    super.initState();
+    checkInitialInternetConnection();
+    checkInternetConnectivity();
+  }
+
+  late final StreamSubscription _connectivityStatusStream;
+
+  void checkInitialInternetConnection() async {
+    final result = await Connectivity().checkConnectivity();
+    handleConnectivityState(result);
+  }
+
+  void checkInternetConnectivity() {
+    _connectivityStatusStream =
+        Connectivity().onConnectivityChanged.listen((status) {
+      handleConnectivityState(status);
+    });
+  }
+
+  void handleConnectivityState(ConnectivityResult status) {
+    if (status != ConnectivityResult.mobile &&
+        status != ConnectivityResult.wifi) {
+      Get.showSnackbar(const GetSnackBar(
+          title: 'Not Internet',
+          message: 'Please connect with internet',
+          isDismissible: false));
+    } else {
+      if (Get.isSnackbarOpen) {
+        Get.closeAllSnackbars();
+      }
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       navigatorKey: CraftyBay.globalKey,
@@ -23,28 +62,59 @@ class _CraftyBayState extends State<CraftyBay> {
       debugShowCheckedModeBanner: false,
       home: const SplashScreen(),
       theme: ThemeData(
-        primarySwatch:
-            MaterialColor(AppColors.primaryColor.value, AppColors().color),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            textStyle: const TextStyle(
-                fontSize: 16, letterSpacing: 0.5, fontWeight: FontWeight.w600),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8),
+          primarySwatch:
+              MaterialColor(AppColors.primaryColor.value, AppColors().color),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              textStyle: const TextStyle(
+                  fontSize: 16,
+                  letterSpacing: 0.5,
+                  fontWeight: FontWeight.w600),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
             ),
           ),
-        ),
-        inputDecorationTheme: const InputDecorationTheme(
-          contentPadding: EdgeInsets.symmetric(horizontal: 16),
-          border: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey)),
-          enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey)),
-          disabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey)),
-
-        )
-      ),
+          inputDecorationTheme: const InputDecorationTheme(
+            contentPadding: EdgeInsets.symmetric(horizontal: 16),
+            border:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+            enabledBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+            disabledBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+          )),
+      darkTheme: ThemeData(
+        brightness: Brightness.light,
+          primarySwatch:
+              MaterialColor(AppColors.primaryColor.value, AppColors().color),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              textStyle: const TextStyle(
+                  fontSize: 16,
+                  letterSpacing: 0.5,
+                  fontWeight: FontWeight.w600),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+          ),
+          inputDecorationTheme: const InputDecorationTheme(
+            contentPadding: EdgeInsets.symmetric(horizontal: 16),
+            border:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+            enabledBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+            disabledBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+          )),
+      themeMode: ThemeMode.dark,
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _connectivityStatusStream.cancel();
   }
 }
